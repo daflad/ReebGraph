b1c36ceb308a54685b8f55f54b0863e6a9b78075
Stephen John Russell <stephenjohnrussell@me.com>
2014-03-14 12:07:34 +0000

    Purged excess files

--- ReebGraph/Step1.build/Debug/ALL_BUILD.build/Script-A5EC0F934DE24DA6AF351B00.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/sjr/dev/ReebGraph/ReebGraph -f /Users/sjr/dev/ReebGraph/ReebGraph/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- ReebGraph/Step1.build/Debug/ZERO_CHECK.build/Script-620358C97BB44DD8A7034439.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/sjr/dev/ReebGraph/ReebGraph -f /Users/sjr/dev/ReebGraph/ReebGraph/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
